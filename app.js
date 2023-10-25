61939d1ba4309aab8f98dd1cb27767911f8d83f9
christyuda <yudacihuy09@gmail.com>
2023-10-25 21:22:50 +0700

    push beranda

--- app.js
+++ app.js
@@ -27,6 +27,37 @@ app.use("/api", bookRoutes);
 // Random Books API Routes
 app.use("/api", randomBooksRoutes);
 
+// Rute beranda
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Selamat datang di API buku kita</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+          text-align: center;
+          padding: 50px;
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #666;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Selamat datang di API buku kita!</h1>
+      <p>Ini adalah backend untuk aplikasi layanan buku.</p>
+    </body>
+    </html>
+  `);
+});
+
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
 });
