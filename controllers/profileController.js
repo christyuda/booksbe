d77a2776db8d8a729e1922976443123414fe969a
christyuda <yudacihuy09@gmail.com>
2023-10-29 21:31:13 +0700

    get, get by id, update dan delete

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -17,10 +17,33 @@ const createProfile = async (req, res) => {
   }
 };
 
-// Mendapatkan profil berdasarkan ID
+// Mendapatkan semua profil
+const getAllProfiles = async (req, res) => {
+  try {
+    const profiles = await Profile.find();
+
+    // Status code 200: OK jika berhasil mendapatkan semua profil
+    res.status(200).json(profiles);
+  } catch (error) {
+    // Status code 500: Internal Server Error jika terjadi kesalahan
+    res.status(500).json({
+      error: "Terjadi kesalahan saat mengambil semua profil",
+      errorMessage: error.message,
+    });
+  }
+};
+
 const getProfileById = async (req, res) => {
   try {
-    const profile = await Profile.findById(req.params.id);
+    const profileId = req.query._id;
+
+    if (!profileId) {
+      // Status code 400: Bad Request jika parameter query _id tidak diberikan
+      res.status(400).json({ error: "Parameter query _id diperlukan" });
+      return;
+    }
+
+    const profile = await Profile.findById(profileId);
 
     if (!profile) {
       // Status code 404: Not Found jika profil tidak ditemukan
@@ -32,13 +55,81 @@ const getProfileById = async (req, res) => {
     res.status(200).json(profile);
   } catch (error) {
     // Status code 500: Internal Server Error jika terjadi kesalahan
-    res
-      .status(500)
-      .json({
-        error: "Terjadi kesalahan saat mengambil profil",
-        errorMessage: error.message,
-      });
+    res.status(500).json({
+      error: "Terjadi kesalahan saat mengambil profil",
+      errorMessage: error.message,
+    });
   }
 };
 
-module.exports = { createProfile, getProfileById };
+// Kontroler untuk mengupdate profil berdasarkan _id dari parameter query
+const updateProfile = async (req, res) => {
+  try {
+    const { name, email, address, password } = req.body;
+    const profileId = req.query._id;
+
+    if (!profileId) {
+      // Status code 400: Bad Request jika parameter query _id tidak diberikan
+      return res.status(400).json({ error: "Parameter query _id diperlukan" });
+    }
+
+    const updatedProfile = await Profile.findByIdAndUpdate(profileId, {
+      name,
+      email,
+      address,
+      password,
+    });
+
+    if (!updatedProfile) {
+      // Status code 404: Not Found jika profil tidak ditemukan
+      return res.status(404).json({ error: "Profil tidak ditemukan" });
+    }
+
+    // Status code 200: OK jika profil berhasil diupdate
+    res
+      .status(200)
+      .json({ message: "Profil berhasil diupdate", updatedProfile });
+  } catch (error) {
+    // Status code 500: Internal Server Error jika terjadi kesalahan
+    res.status(500).json({
+      error: "Terjadi kesalahan saat mengupdate profil",
+      errorMessage: error.message,
+    });
+  }
+};
+const deleteProfile = async (req, res) => {
+  try {
+    const profileId = req.query._id;
+
+    if (!profileId) {
+      // Status code 400: Bad Request jika parameter query _id tidak diberikan
+      return res.status(400).json({ error: "Parameter query _id diperlukan" });
+    }
+
+    const deletedProfile = await Profile.findByIdAndRemove(profileId);
+
+    if (!deletedProfile) {
+      // Status code 404: Not Found jika profil tidak ditemukan
+      return res.status(404).json({ error: "Profil tidak ditemukan" });
+    }
+
+    // Status code 200: OK jika profil berhasil dihapus
+    res
+      .status(200)
+      .json({ message: "Profil berhasil dihapus", deletedProfile });
+  } catch (error) {
+    // Status code 500: Internal Server Error jika terjadi kesalahan
+    res.status(500).json({
+      error: "Terjadi kesalahan saat menghapus profil",
+      errorMessage: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createProfile,
+  getAllProfiles,
+  getProfileById,
+  updateProfile,
+  deleteProfile,
+};
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,13 +2,25 @@ const express = require("express");
 const router = express.Router();
 const {
   createProfile,
+  getAllProfiles,
   getProfileById,
+  updateProfile,
+  deleteProfile,
 } = require("../controllers/profileController");
 
-// Rute untuk membuat profil
-router.post("/profile", createProfile);
+// Rute untuk membuat profil baru
+router.post("/profile/add", createProfile);
+
+// Rute untuk mendapatkan semua profil
+router.get("/profile", getAllProfiles);
 
 // Rute untuk mendapatkan profil berdasarkan ID
 router.get("/profile/:id", getProfileById);
+
+// Rute untuk mengupdate profil berdasarkan ID
+router.put("/profile/update", updateProfile);
+
+// Rute untuk menghapus profil berdasarkan ID
+router.delete("/profile/delete", deleteProfile);
 
 module.exports = router;
